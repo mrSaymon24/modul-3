21947c9b991b7282f499a8c8eb146f52a601188e
mrSaymon24 <pisyaka95@gmail.com>
2024-10-22 21:11:09 +0700

    Add files via upload

---
+++ modul 3.2.py
@@ -0,0 +1,18 @@
+def send_email (message, recipient, *, sender="university.help@gmail.com"):
+    def is_vailed_email (email):
+        return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
+    if not is_vailed_email (sender) or not is_vailed_email(recipient):
+        print(f"невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+        return
+    if sender == recipient:
+        print("нельзя отправить письмо самому себе")
+        return
+    if sender == "university.help@gmail.com":
+        print(f"письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
